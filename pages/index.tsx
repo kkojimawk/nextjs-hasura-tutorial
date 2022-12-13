faf17ddf182f14a3ee415be80a05ad982364c095
kkojimawk <kkojimawk@gmail.com>
2022-12-14 01:05:52 +0900

    Layout作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,12 @@
-import Head from 'next/head'
+import { VFC } from 'react'
+import { Layout } from '../components/Layout'
 
-export default function Home() {
-  return <div>init</div>
+const Home: VFC = () => {
+  return (
+    <Layout title="Home">
+      <p className="text-3xl font-bold">Next.js + GraphQL</p>
+    </Layout>
+  )
 }
+
+export default Home
